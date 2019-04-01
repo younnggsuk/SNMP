99b305dc7b326d3a0370872d5e18a24d74b83958
younnggsuk <younnggsuk@naver.com>
2019-04-01 18:42:01 +0900

    SNMP

--- snmp++/example.cpp
+++ snmp++/example.cpp
@@ -7,115 +7,35 @@
 using namespace Snmp_pp;
 using namespace std;
 
-///////////////////////////////////////////////////////////
-//class SnmpResult
-//{
-//private:
-//	char *oid;
-//	char *value;
-//public:	
-//	SnmpResult();
-//	SnmpResult(const char *_oid, const char *_value);
-//	SnmpResult(const SnmpResult &copy);
-//	void SetOid(ceonst char *_oid);
-//	void SetValue(const char *_value);
-//	const char* GetOid() const;
-//	const char* GetValue() const;
-//	void PrintOid() const;
-//	void PrintValue() const;
-//	~SnmpResult();
-//};
-//
-//SnmpResult::SnmpResult() : oid(NULL), value(NULL)
-//{ } 
-//SnmpResult::SnmpResult(const char *_oid, const char *_value)
-//{
-//	oid = new char[strlen(_oid)+1];
-//	value = new char[strlen(_value)+1];
-//	strcpy(oid, _oid);
-//	strcpy(value, _value);
-//}
-//SnmpResult::SnmpResult(const SnmpResult &copy)
-//{
-//	oid = new char[strlen(copy.oid)+1];
-//	value = new char[strlen(copy.value)+1];
-//	strcpy(oid, copy.oid);e
-//	strcpy(value, copy.value);
-//}
-//void SnmpResult::SetOid(const char *_oid)
-//{
-//	if(oid != NULL) {
-//		delete []oid;
-//	}
-//	oid = new char[strlen(_oid)+1];
-//	strcpy(oid, _oid);
-//}
-//void SnmpResult::SetValue(const char *_value)
-//{
-//	if(value != NULL) {
-//		delete []value;
-//	}
-//	value = new char[strlen(_value)+1];
-//	strcpy(value, _value);
-//}
-//const char* SnmpResult::GetOid() const
-//{
-//	return oid;
-//}e
-//const char* SnmpResult::GetValue() const
-//{
-//	return value;
-//}
-//void SnmpResult::PrintOid() const
-//{
-//	cout<<oid<<endl;
-//}
-//void SnmpResult::PrintValue() const
-//{
-//	cout<<value<<endl;
-//}
-//SnmpResult::~SnmpResult()
-//{
-//	if(oid != NULL) {
-//		delete []oid;
-//	}
-//	if(value != NULL) {
-//		delete []value;
-//	}
-//}
-///////////////////////////////////////////////
-
 #define IP_ADDR_ENTRY_TABLE			"1.3.6.1.2.1.4.20.1.1"
 #define IP_NET_TO_MEDIA_NET_ADDR	"1.3.6.1.2.1.4.22.1.3"
 
-int SnmpGetNext(const char *ip, const char *community, Vb &vb, int oidIdx);
-void RecursiveGet(const char *ip, const char *community, Vb &curVb, int oidIdx);
+int SnmpGetNext(const char *ip, const char *community, Vb &vb, int len);
 
 int main(int argc, char *argv[])
 {
-	Oid oid(IP_ADDR_ENTRY_TABLE);
-	Vb curVb(oid);
-	RecursiveGet("127.0.0.1", "public", curVb, oid.len());
-	
+	if(argc != 3) {
+		cout<<"Usage : "<<argv[0]<<" <IP> <COMMUNITY STRING>\n";
+		return 0;
+	}
 
+	int status;
+	Vb curVb(IP_ADDR_ENTRY_TABLE);
+	if((status = SnmpGetNext("127.0.0.1", argv[2], curVb, strlen(curVb.get_printable_oid()))) < 0) {
+		Snmp::error_msg(status);
+	}
+
+	cout<<endl<<endl;
+
+	Vb nextVb(IP_NET_TO_MEDIA_NET_ADDR);
+	if((status = SnmpGetNext(argv[1], argv[2], nextVb, strlen(nextVb.get_printable_oid()))) < 0) {
+		Snmp::error_msg(status);
+	}
 
 	return 0;
 }
 
-void RecursiveGet(const char *ip, const char *community, Vb &curVb, int oidIdx)
-{
-	int status = SnmpGetNext(ip, community, curVb, oidIdx);
-	
-	if(status < 0) {
-		cout<<Snmp::error_msg(status)<<endl;
-		return;
-	}
-
-	Vb nextVb(curVb.get_printable_oid());
-	RecursiveGet(curVb.get_printable_value(), community, nextVb, oidIdx);
-}
-
-int SnmpGetNext(const char *ip, const char *community, Vb &vb, int oidIdx)
+int SnmpGetNext(const char *ip, const char *community, Vb &vb, int len)
 {
 	int status;
 
@@ -125,22 +45,27 @@ int SnmpGetNext(const char *ip, const char *community, Vb &vb, int oidIdx)
 	
 	Snmp snmp(status);
 	if(status != SNMP_CLASS_SUCCESS) {
+		snmp.error_msg(status);
 		return status;
 	}
 
 	pdu += vb;
 	if((status = snmp.get_next(pdu, ctarget)) != SNMP_CLASS_SUCCESS){
+		snmp.error_msg(status);
 		return status;
 	}
 
-	auto cur = vb.get_printable_oid()[oidIdx+1];
+	int prevOidEnd = vb.get_printable_oid()[len-1];
 	pdu.get_vb(vb, 0);
-	auto next = vb.get_printable_oid()[oidIdx+1];
-
-	cout<<"cur : "<<cur<<", "<<next<<endl;
+	if(prevOidEnd != vb.get_printable_oid()[len-1]) {
+		return 0;
+	}
 
 	cout<<vb.get_printable_oid()<<endl;
 	cout<<vb.get_printable_value()<<endl;
+
+	Vb nextVb(vb.get_printable_oid());
+	SnmpGetNext(vb.get_printable_value(), community, nextVb, len);
 
 	return 0;
 }
