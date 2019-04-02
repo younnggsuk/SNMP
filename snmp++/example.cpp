e26f30401744f0ebeb779bcdcf559fea71959d97
younnggsuk <younnggsuk@naver.com>
2019-04-02 19:06:35 +0900

    SNMP

--- snmp++/example.cpp
+++ snmp++/example.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #define IP_ADDR_ENTRY_TABLE			"1.3.6.1.2.1.4.20.1.1"
 #define IP_NET_TO_MEDIA_NET_ADDR	"1.3.6.1.2.1.4.22.1.3"
 
-int SnmpGetNext(const char *ip, const char *community, Vb &vb, int len, vector<string> &arr);
+int SnmpGetNext(const char *ip, const char *community, Vb &vb, vector<string> &buf);
 
 int main(int argc, char *argv[])
 {
@@ -19,31 +19,26 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	vector<string> arr;
+	vector<string> ipAddr;
+	Oid oid(IP_ADDR_ENTRY_TABLE);
+	Vb vb(oid);
+	SnmpGetNext(argv[1], argv[2], vb, ipAddr);
+	cout<<"-------------------------------------"<<endl;
+	cout<<"-------------------------------------"<<endl;
 
-	int status;
-	Vb curVb(IP_ADDR_ENTRY_TABLE);
-	if((status = SnmpGetNext("127.0.0.1", argv[2], curVb, strlen(curVb.get_printable_oid()), arr)) < 0) {
-		Snmp::error_msg(status);
+	vector<string> hops;
+	Oid oid2(IP_NET_TO_MEDIA_NET_ADDR);
+	Vb vb2(oid2);
+	for(auto i : ipAddr) {
+		SnmpGetNext(i.c_str(), argv[2], vb2, hops);
+		cout<<"-------------------------------------"<<endl;
+		cout<<"-------------------------------------"<<endl;
 	}
 
-	cout<<endl<<endl;
-
-	Vb nextVb(IP_NET_TO_MEDIA_NET_ADDR);
-	int ipNum = arr.size();
-	for(int i=0; i<ipNum; i++) {
-		if((status = SnmpGetNext(arr[i].c_str(), argv[2], nextVb, strlen(nextVb.get_printable_oid()), arr)) < 0) {
-			Snmp::error_msg(status);
-		}
-	}
-
-	for(auto i : arr) {
-		cout<<i<<endl;
-	}
 	return 0;
 }
 
-int SnmpGetNext(const char *ip, const char *community, Vb &vb, int len, vector<string> &arr)
+int SnmpGetNext(const char *ip, const char *community, Vb &vb, vector<string> &buf)
 {
 	int status;
 
@@ -53,24 +48,25 @@ int SnmpGetNext(const char *ip, const char *community, Vb &vb, int len, vector<s
 	
 	Snmp snmp(status);
 	if(status != SNMP_CLASS_SUCCESS) {
-		return status;
+		snmp.error_msg(status);
+		return -1;
 	}
 
 	pdu += vb;
-	if((status = snmp.get_next(pdu, ctarget)) != SNMP_CLASS_SUCCESS){
-		return status;
+	if((status = snmp.get_bulk(pdu, ctarget, 0, 20)) == SNMP_CLASS_SUCCESS) {
+		for(int i=0; i<pdu.get_vb_count(); i++) {
+			pdu.get_vb(vb, i);
+			if(vb.get_syntax() == 2) {
+				break;
+			}
+			cout<<vb.get_printable_value()<<endl;
+			buf.push_back(vb.get_printable_value());
+		}
 	}
-
-	int prevOidEnd = vb.get_printable_oid()[len-1];
-	pdu.get_vb(vb, 0);
-	if(prevOidEnd != vb.get_printable_oid()[len-1]) {
-		return 0;
+	else { 
+		snmp.error_msg(status);
+		return -1;
 	}
-
-	arr.push_back(vb.get_printable_value());
-
-	Vb nextVb(vb.get_printable_oid());
-	SnmpGetNext(vb.get_printable_value(), community, nextVb, len, arr);
 
 	return 0;
 }
