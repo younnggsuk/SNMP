77ac92905e8ae6f7e8032a30039210c4500a6afd
younnggsuk <younnggsuk@naver.com>
2019-04-04 19:10:56 +0900

    SNMP

--- snmp++/example.cpp
+++ snmp++/example.cpp
@@ -5,13 +5,17 @@
 #include <map>
 #include <algorithm>
 
+namespace OID
+{
+	const char* ipRouteNextHop 	= "1.3.6.1.2.1.4.21.1.7";
+	const char* ipRouteType 	= "1.3.6.1.2.1.4.21.1.8";
+}
+
 using namespace Snmp_pp;
 using namespace std;
 
-#define IP_ADDR_ENTRY_TABLE			"1.3.6.1.2.1.4.20.1.1"
-#define IP_NET_TO_MEDIA_NET_ADDR	"1.3.6.1.2.1.4.22.1.3"
-
 int SnmpGetNext(const char *ip, const char *community, Vb &vb, vector<string> &buf);
+void GetAllNextIp(const char *ip, const char *community, vector<string> &buf);
 
 int main(int argc, char *argv[])
 {
@@ -20,40 +24,46 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	vector<string> ipAddr;
-	Oid oid(IP_ADDR_ENTRY_TABLE);
-	Vb vb(oid);
-	if(SnmpGetNext(argv[1], argv[2], vb, ipAddr) < 0) {
-		cout<<"SnmpGetNextError"<<endl;
-		return 0;
-	}
-	
-	map<string, string> l1;
-	for(auto i : ipAddr) {
-		l1[i] = "";
-	}
+	vector<string> nextIp;
+	GetAllNextIp(argv[1], argv[2], nextIp);
 
-	Oid oid2(IP_NET_TO_MEDIA_NET_ADDR);
-	Vb vb2(oid2);
-	for(auto i : ipAddr) {
-		vector<string> buf;
-		SnmpGetNext(i.c_str(), argv[2], vb2, buf);
-		cout<<i<<' ';
-		l1[i] = buf.back();
-	}
-	cout<<endl;
-
-	for(auto i = l1.begin(); i != l1.end(); i++) {
-		cout<<(*i).first<<"-->>"<<(*i).second<<endl;
+	for(auto i : nextIp) {
+		cout<<i<<endl;
 	}
 
 	return 0;
 }
 
+void GetAllNextIp(const char *ip, const char *community, vector<string> &buf)
+{
+	vector<string> nextHop;
+	Vb vbHop(OID::ipRouteNextHop);
+	if(SnmpGetNext(ip, community, vbHop, nextHop) < 0) {
+		cout<<"SnmpGetNextError"<<endl;
+		return;
+	}	
+
+	vector<string> type;
+	Vb vbType(OID::ipRouteType);
+	if(SnmpGetNext(ip, community, vbType, type) < 0) {
+		cout<<"SnmpGetNextError"<<endl;
+		return;
+	}	
+
+	int idx = 0;
+	for(auto i : type) {
+		if(i == "4") {
+			buf.push_back(nextHop[idx]);
+		}
+		idx++;
+	}
+}
+
 int SnmpGetNext(const char *ip, const char *community, Vb &vb, vector<string> &buf)
 {
 	int status;
-
+	string curOid(vb.get_printable_oid());
+	
 	CTarget ctarget((IpAddress)ip, community, community);
 	ctarget.set_version(version2c);
 	Pdu pdu;
@@ -68,10 +78,10 @@ int SnmpGetNext(const char *ip, const char *community, Vb &vb, vector<string> &b
 	if((status = snmp.get_bulk(pdu, ctarget, 0, 20)) == SNMP_CLASS_SUCCESS) {
 		for(int i=0; i<pdu.get_vb_count(); i++) {
 			pdu.get_vb(vb, i);
-			if(vb.get_syntax() == 2) {
+			if(curOid[curOid.size()-1] != vb.get_printable_oid()[curOid.size()-1]) {
 				break;
 			}
-			cout<<vb.get_printable_value()<<endl;
+			//cout<<vb.get_printable_value()<<endl;
 			buf.push_back(vb.get_printable_value());
 		}
 	}
